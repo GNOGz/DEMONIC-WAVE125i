06386ee836f0a7120c29980e3d94a216fa9c9643
GNOGz <gong6321@gmail.com>
2025-10-12 22:45:42 +0700

    test tailwind

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,13 +1,30 @@
 <html lang="en">
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <div>
-        <div class="">Hello world 555</div>
+
+<body class=" border-8 border-slate-400">
+    <div class="text-slate-500 m-5 grid grid-flow-row gap-2 ">
+        <div class="border-2 border-slate-300 text-4xl grid grid-flow-col align-middle text-center">
+            Register
+        </div>
+        <div class="flex gap-3 align-middle items-center">
+            <div>Name</div>
+            <input type="text" placeholder="">
+        </div>
+        <div class="flex gap-3 align-middle items-center">
+            <div>Email</div>
+            <input type="text" placeholder="">
+        </div>
+        <div class="flex gap-3 align-middle items-center">
+            <div>Password</div>
+            <input type="text" placeholder="">
+        </div>
     </div>
-    <div>Whassup</div>
 </body>
+
 </html>
