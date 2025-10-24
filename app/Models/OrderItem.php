997d0ba5fdecb730c7f01c3cd1132e0a111f26bb
Vualerie <jumper13577@gmail.com>
2025-10-24 19:35:58 +0700

    model orderitem,product(function orderitem)

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
-    //
+    protected $fillable = [
+        'quantity',
+        'price',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'order_id');
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -24,4 +24,9 @@ class Product extends Model
     {
         return $this->hasMany(Cart::class, 'product_id');
     }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class, 'product_id');
+    }
 }
