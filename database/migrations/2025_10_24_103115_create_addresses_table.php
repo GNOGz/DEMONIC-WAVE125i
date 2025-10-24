952917dba64c006ff2d05d8b7e318f603b09772b
GNOGz <gong6321@gmail.com>
2025-10-24 17:48:15 +0700

    add product,user,address migration

---
+++ database/migrations/2025_10_24_103115_create_addresses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('postal_code');
+            $table->string('phone_number',10);
+            $table->string('province');
+            $table->string('distric');
+            $table->string('detail')->default('');
+            $table->foreign('id')->references('id')->on('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
