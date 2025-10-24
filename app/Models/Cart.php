8479f589100aa3bed14760ad466c2034c53525a9
Vualerie <jumper13577@gmail.com>
2025-10-24 19:23:18 +0700

    model cart,category,product,user(cart)

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cart extends Model
 {
-    //
+    protected $fillable = [
+        'product_id',
+        'user_id',
+        'quantity',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'price',
+        'in_stock',
+        'description',
+        'image_url',
+        'category',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function carts()
+    {
+        return $this->hasMany(Cart::class, 'product_id');
+    }
 }
