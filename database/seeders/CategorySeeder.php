c060aec57b115d14b355c0ed0e5034bacce734f1
navyney <boontharika2548@gmail.com>
2025-10-24 19:57:37 +0700

    Category Seeder v1.0

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -13,6 +13,24 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $categories = [
+            ['name' => 'Engine', 'description' => 'The core power source that drives performance and energy.'],
+            ['name' => 'Frame', 'description' => 'The structural backbone that ensures stability and support.'],
+            ['name' => 'Rear Suspension', 'description' => 'Provides comfort and control, absorbing shocks from every journey.'],
+            ['name' => 'Power Pipe', 'description' => 'Enhances performance and efficiency, channeling energy effectively.'],
+            ['name' => 'Spoke Wheel', 'description' => 'A lightweight, sturdy spoke wheel that ensures smooth rotation and evenly distributes load.'],
+        ];
+
+
+        foreach ($categories as $category) {
+            Category::firstOrCreate(
+                ['name' => $category['name']], // search by unique column
+                [
+                    'description' => $category['description'],
+                    'created_at'  => Carbon::now(),
+                    'updated_at'  => Carbon::now(),
+                ]
+            );
+        }
     }
 }
