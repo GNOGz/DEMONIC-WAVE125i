63459ecdbebc91abecafe5b9ba1c43497ceb4e95
GNOGz <gong6321@gmail.com>
2025-10-26 03:07:29 +0700

    complete seeder for product and category

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -13,23 +13,13 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        $categories = [
-            ['name' => 'Engine', 'description' => 'The core power source that drives performance and energy.'],
-            ['name' => 'Frame', 'description' => 'The structural backbone that ensures stability and support.'],
-            ['name' => 'Rear Suspension', 'description' => 'Provides comfort and control, absorbing shocks from every journey.'],
-            ['name' => 'Power Pipe', 'description' => 'Enhances performance and efficiency, channeling energy effectively.'],
-            ['name' => 'Spoke Wheel', 'description' => 'A lightweight, sturdy spoke wheel that ensures smooth rotation and evenly distributes load.'],
-        ];
+        \DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        Category::truncate();
+        \DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        $categories = ['Engine', 'Frame', 'Rear Suspension', 'Power Pipe', 'Spoken Wheel'];
 
-        foreach ($categories as $category) {
-            Category::firstOrCreate(
-                ['name' => $category['name']], // search by unique column
-                [
-                    'description' => $category['description'],
-                    'created_at'  => Carbon::now(),
-                    'updated_at'  => Carbon::now(),
-                ]
-            );
+        foreach ($categories as $name) {
+            Category::create(['category_name' => $name]);
         }
     }
 }
