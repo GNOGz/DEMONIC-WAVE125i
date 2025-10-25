adb165ed6137731c9f3ce0e817c75118fa260b14
GNOGz <gong6321@gmail.com>
2025-10-26 01:52:26 +0700

    user can now add photo and user now have default profile photo

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -5,8 +5,13 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+    <div class="max-w-5xl mx-auto sm:px-6 lg:px-8 space-y-6 my-6">
+        <div class="ml-8 max-w-4xl p-4 sm:p-8 bg-white  shadow sm:rounded-lg">
+            <div class="max-w-xl">
+                @include('profile.partials.update-profile-photo-form')
+            </div>
+        </div>
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -15,6 +15,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::patch('/profile/photo/update', [UserController::class, 'updateProfilePhoto'])->name('profile.photo.update');
+    Route::get('/profile/photo/{filename}', [UserController::class, 'showProfilePhoto'])->where('filename', '.*')->name('user.photo');
+
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
